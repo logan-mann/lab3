577ce269f9dd67df2eb891de6e1c0b259f236671
logan-mann <loganmann8@gmail.com>
2019-09-26 19:00:26 -0600

    Adding email results to the respository

--- lab3_script.sh
+++ lab3_script.sh
@@ -16,16 +16,17 @@ grep $regExpression $fileName
 
 #string starts with 3 digits followed by a hyphen followed by 3 more digits, another hyphen and 4
 #more digits
-grep -c "^[0-9]{3}-[0-9]{3}-[0-9]{4}$" regex_practice.txt
+grep -c -P [0-9]{3}-[0-9]{3}-[0-9]{4}$ regex_practice.txt
 
 #prints the count of lines that contain an @ followed by at least one alphabetical character wollowed by a . followed by at least one alphabetical char
-grep -c "@[a-zA-Z]+\.[a-zA-Z]+$" regex_practice.txt
+grep -c -P .@.+/.+ regex_practice.txt
 
 
 #prints matching parts of lines that contain 303 followed by a dash and any 9 digit phone number
-grep -o "^303-[0-9]{3}-\[0-9]{4}$" regex_practice.txt
 
-grep "geocities.com" regex_practice.txt >> email_results.txt
+grep -o -P 303-[0-9]{3}-[0-9]{4} regex_practice.txt
+
+grep ".@geocities.com" regex_practice.txt >> email_results.txt
 
 git init
 
